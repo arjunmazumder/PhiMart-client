32f43369bbc9b1c719b871c952778b9b180e9ec4
Arjun Mazumder <arjumazumder2@gmail.com>
2025-12-03 11:17:46 +0600

    m29.2 updating state object & nested object state

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,13 +2,44 @@ import { useState } from "react";
 
 
 const Person = () => {
-    const [person, setPerson] = useState("Tani")
+    const [person, setPerson] = useState(
+        {
+            firstName: "",
+            lastName: "",
+            email: "",
+            address: {
+                city: "Dhaka",
+                thana: "Addabor",
+            }
+
+        }
+        
+    )
+
     const handleClick = () =>{
-         setPerson("Arjun")
-         console.log(person)
+         const newPerson = {
+            ...person,
+            firstName: "Arjun",
+            lastName: "Mazmder",
+            email: "arjun@gmail.com",
+            address:{
+                ...person,
+                city: "Barisal"
+            }
+            
+            
+
+         };
+
+         setPerson(newPerson);
+
+
     }
     return (
         <div className="m-5"> 
+        <p>{person.firstName} {person.lastName}</p>
+         <p>{person.email}</p>
+         <h1>{person.address.city}</h1>
 
             <button onClick={handleClick} className="px-3 py-2 bg-blue-500 text-white rounded-sm">Click Me</button>
             
