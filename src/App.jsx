2c25327be0f82298d6ceccc5f397d6babfa9cad4
Arjun Mazumder <arjumazumder2@gmail.com>
2025-12-16 20:53:16 +0600

    module29.6 introduction to effect hook and understanding strictMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react';
-import './App.css'
-import Cart from './components/Cart';
-import Navbar from './components/Navbar';
+
+import './App.css';
+import Effect from './components/Effect';
 
 
 
 function App() {
 
-const [CartItems, SetCartItems] = useState(['product 1', 'product 2', 'product 3'])
+
   return (
     <div>
-      <Navbar CartItemsCount={CartItems.length} />
-      <Cart CartItems={CartItems} />
+     <Effect />
 
     
     </div>
